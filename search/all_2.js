97a2649cdcd31a33dac16388d7b3752a3bd7163b
TalkedDevotee <jokerpokerface@yandex.ru>
2017-10-19 23:24:36 +0300

    added documentation

---
+++ search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['the_20travis_20client_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2ftalkeddevotee_2flab07_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2ftalkeddevotee_2flab07_29',['The Travis Client [![Build Status](https://travis-ci.org/TalkedDevotee/lab07.svg?branch=master)](https://travis-ci.org/TalkedDevotee/lab07)',['../index.html',1,'']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "prt",
+  1: "pr",
+  2: "p",
+  3: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Файлы",
+  2: "Функции",
+  3: "Страницы"
+};
+
